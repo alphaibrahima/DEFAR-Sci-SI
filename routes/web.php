2dded2f287d501333a7efa3fa00c675d144f405a
Al-Amine <ndoyeteranga777@gmail.com>
2023-03-15 12:50:31 +0000

    espacement dans web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+
+
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
