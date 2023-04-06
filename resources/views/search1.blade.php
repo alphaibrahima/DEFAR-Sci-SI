44daf802511e81852914275cd6ec0c98a1ee292a
mamejarrah <diarragueye799@gmail.com>
2023-04-06 15:33:41 +0200

    3 Download done

--- resources/views/search1.blade.php
+++ resources/views/search1.blade.php
@@ -11,6 +11,7 @@
 </style>
 
 <div class="uper">
+    @include('partials.dev')
 
   @if(session()->get('success'))
     <div class="alert alert-success">
@@ -25,9 +26,12 @@
           <td>ID</td>
           <td>Nom</td>
           <td>Prenom</td>
-          <td >Show</td>
-          <td>Edit</td>
-          <td>Delete</td>
+          <td >Montrer</td>
+          <td>Modifier</td>
+          <td>Supprimer</td>
+          <td>Telecharger</td>
+           
+
         </tr>
     </thead>
 
@@ -38,17 +42,33 @@
       <td>{{ $entretien->nom}}</td>
       <td>{{ $entretien->prenom}}</td>
 
-            <td><a href="{{ route('entretiens.show', $entretien->id)}}" class="btn btn-info">Show</a></td>
 
-             <td><a href="{{ route('entretiens.edit', $entretien->id)}}" class="btn btn-primary" >Edit</a></td>
+
+            <td><a href="{{ route('entretiens.show', $entretien->id)}}" class="btn btn-success w-9 h-8" style=" margin-top:20%"><i class="fa fa-eye " aria-hidden="true"></i></a></td>
+
+             <td><a href="{{ route('entretiens.edit', $entretien->id)}}" class="btn btn-primary w-9 h-8" style=" margin-top:20%" ><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
 
             <td>
                 <form action="{{ route('entretiens.destroy', $entretien->id)}}" method="post">
                   @csrf
                   @method('DELETE')
-                  <button  class="btn btn-danger" type="submit">Delete</button>
+                  <button  class="btn btn-danger w-9 h-8" style=" margin-top:20%" type="submit"><i class="fa fa-trash" aria-hidden="true"></i></button>
                 </form>
             </td>
+
+
+            <td>
+                 <a class="btn btn-warning w-9 h-8" style=" margin-top:20%"  href="{{ route('entretien.pdf') }}"> <i class="fa fa-download" aria-hidden="true"></i> </a>
+                
+            </td>
+             
+    
+            
+         
+
+
+
+
         </tr>
         @endforeach
     </tbody>
